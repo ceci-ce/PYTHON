f11a97013965563d54f855a1a6c86f191c23d095
Cecilia <cecilia.delacamara@gmail.com>
2025-03-15 21:42:52 +0100

    Creación Clase3

---
+++ EjerciciosPhyton/Clase3.py
@@ -0,0 +1,10 @@
+#1. Longitud de una cadena
+
+nombre = "Cecilia"
+print("Longitud del nombre: ",len(nombre))
+
+#2. Convertir texto a mayusculas y minusculas
+#upper
+print("Esto soy yo en mayusculas: ",nombre.upper())
+#lower
+print("Esto soy yo en minúsculas: ", nombre.lower())
