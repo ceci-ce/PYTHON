f747f6b012e1bb02974bae90c480935617090539
Cecilia <cecilia.delacamara@gmail.com>
2025-05-02 18:53:05 +0200

    ejercicio 1 nivel básico

---
+++ EjerciciosNivelBasico/ejercicio1.py
@@ -0,0 +1,12 @@
+#Ejercicio 1¶
+#Escribe un programa que pida al usuario un número entero y determine si es par o impar. El programa debe imprimir un
+#mensaje indicando el resultado.
+
+numero = int(input("Dime un numero: "))
+
+if numero % 2 == 0:
+    print ("El numero es par")
+else:
+    print ("El numero es impar")
+
+
