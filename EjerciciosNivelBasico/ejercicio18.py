06204142cecbd9d1d963bb7b69e8b9fb0a51b8e6
Cecilia <cecilia.delacamara@gmail.com>
2025-05-21 19:10:27 +0200

    ejercicio 18 resuelto con listas

--- EjerciciosNivelBasico/ejercicio18.py
+++ EjerciciosNivelBasico/ejercicio18.py
@@ -2,8 +2,16 @@
 Escribe un programa que dado una serie de números introducidos por el usuario, hasta que introduzca un -1, imprima el
 número introducido sumándole 1. El programa debe imprimir todos los números introducidos, menos el -1, sumándoles 1."""
 
+
+numeros = []
+
 while True:
     numero = int(input("Introduce un número (-1 para salir): "))
     if numero == -1:
         break
-    print(numero + 1)
+    numeros.append(numero)
+
+    # metodo de las listas en Python que añade un elemento al final.
+
+for n in numeros:
+    print(n + 1)
