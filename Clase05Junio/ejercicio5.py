3aeb0c780d7e6aed43ec8261eaefc25f197ca12e
Cecilia <cecilia.delacamara@gmail.com>
2025-06-06 20:55:15 +0200

    ejemplo

--- Clase05Junio/ejercicio5.py
+++ Clase05Junio/ejercicio5.py
@@ -7,9 +7,30 @@ función que reciba el número, realice el cálculo de la suma de los dígitos y
 def suma_digitos(numero):
     resultado = 0
     while numero > 0:
-        resultado += numero%10
-        numero //= 10
+        resultado += numero%10 # Obtiene el último dígito del número.
+        numero //= 10 #Elimina el último dígito (división entera entre 10).
     return resultado
 
 numero = int(input("Introduce un numero entero positivo: "))
 print(f"La suma de los digitos de {numero} es {suma_digitos(numero)}")
+
+""" EJEMPLO:
+
+Entonces:
+
+472 % 10 = 2
+
+472 // 10 = 47
+
+47 % 10 = 7
+
+47 // 10 = 4
+
+4 % 10 = 4
+
+4 // 10 = 0
+
+Suma total: 2 + 7 + 4 = 13
+
+
+"""
