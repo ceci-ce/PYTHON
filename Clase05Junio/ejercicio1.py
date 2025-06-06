0d62e1fc9108f47b7d6512e52870ed518cf38e29
Cecilia <cecilia.delacamara@gmail.com>
2025-06-06 19:22:44 +0200

    ejercicio 1

---
+++ Clase05Junio/ejercicio1.py
@@ -0,0 +1,12 @@
+"""Ejercicio 1 - Calcular el área de un círculo¶
+Escribe un programa que pida al usuario el radio de un círculo y calcule su área. El programa debe definir una función
+que reciba el valor del radio, realice el cálculo del área y luego imprima el resultado."""
+import math
+
+
+def calcular_area_circulo(radio):
+    area = math.pi * radio**2
+    return area
+
+radio = int(input("Introduce el radio: \n "))
+print(f"El area de un circulo de radio {radio} es {calcular_area_circulo(radio)}")
