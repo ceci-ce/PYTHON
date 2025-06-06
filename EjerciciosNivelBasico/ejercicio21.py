95b26f3f4a9d1e80a5910ccc6feea4c0846686c4
Cecilia <cecilia.delacamara@gmail.com>
2025-06-06 18:14:23 +0200

    enunciado

---
+++ EjerciciosNivelBasico/ejercicio21.py
@@ -0,0 +1,6 @@
+"""Ejercicio 21¶
+Escribe un programa que te introduzca un número entero positivo que corresponde al número de casos a tratar. Seguidamente
+te introducen un número entero positivo que corresponde a una serie de números. Después debes recibir ese total de
+números e imprimirlos en la misma linea de la terminal, separados por un espacio y habiéndoles sumado 1 a cada uno de ellos.
+
+"""
