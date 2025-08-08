c48131d4ce8a768fd217216c4dd1fa997673ff75
Cecilia <cecilia.delacamara@gmail.com>
2025-08-08 20:29:14 +0200

    ejercicio 3 corregido

--- Clase29Mayo/Ejercicio3.py
+++ Clase29Mayo/Ejercicio3.py
@@ -2,17 +2,16 @@
 Escribe un programa que gestione un inventario de productos utilizando un diccionario. El programa debe permitir al
 usuario añadir productos con su nombre y cantidad, eliminar productos, y consultar la cantidad de un producto específico.
 El programa debe ejecutarse indefinidamente hasta que el usuario introduzca "SALIR"."""
-from PYTHON.Clase4.Clase4 import nombre
 
 inventario = {}
 opcion = -1
 
 while opcion != 4:
     print("Escoge una opcion: ")
-    print("1.Añadir producto)
-    print("2.Eliminar producto)
-    print("3.Consultar producto)
-    print("4.Salir)
+    print("1.Añadir producto")
+    print("2.Eliminar producto")
+    print("3.Consultar producto")
+    print("4.Salir")
     opcion = int(input("Introduce una opcion: "))
 
     if opcion == 1:
@@ -26,4 +25,14 @@ while opcion != 4:
             print(f"Producto {nombre} eliminado del inventario")
         else:
             print(f"No existe el producto {nombre} en el inventario")
+    elif opcion == 3:
+        nombre = input("Introduce el producto a consultar: ")
+        if nombre in inventario:
+            print(f"El producto {nombre} tiene una cantidad de {inventario[nombre]} unidades")
+        else:
+            print(f"No existe el producto {nombre} en el inventario")
+    elif opcion == 4:
+        print("Salir.")
+    else:
+        print(f"La opcion {opcion} no existe en la lista de comandos")
 
