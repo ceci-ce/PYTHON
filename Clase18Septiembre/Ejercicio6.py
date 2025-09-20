0ecdac31ea9c0b62e8398a9c56c4d37ceef507f2
Cecilia <cecilia.delacamara@gmail.com>
2025-09-20 19:22:18 +0200

    ejercicio 6

---
+++ Clase18Septiembre/Ejercicio6.py
@@ -0,0 +1,8 @@
+palabras_feas = ["perro", "queso", "examen"]
+frase = input("Dime una frase:\n")
+
+for palabra in palabras_feas:
+    frase = frase.replace(palabra, "*"*len(palabra))
+
+print(frase)
+
