707f1d81d4521f8ab5e918119b75413196c9fa75
Cecilia <cecilia.delacamara@gmail.com>
2025-09-20 19:47:56 +0200

    ejercicio 7

---
+++ Clase18Septiembre/Ejercicio7.py
@@ -0,0 +1,14 @@
+# Analizador de texto
+
+frase =input("Introduce una frase: \n")
+caracteres = len(frase)
+lista = frase.split()
+num_palabras = len(lista)
+mas_larga = ""
+for palabra in lista:
+    if (len(palabra) > len(mas_larga)):
+        mas_larga = palabra
+
+print(f"Número de caracteres: {caracteres}")
+print(f"Número de palabras: {num_palabras}")
+print(f"Palabra más larga: {mas_larga}")
