2160cfdbba70a23975a4a95a908a5cf62fff4aec
Cecilia <cecilia.delacamara@gmail.com>
2025-09-20 19:39:03 +0200

    ejercicio 6 opcion b

--- Clase18Septiembre/Ejercicio6.py
+++ Clase18Septiembre/Ejercicio6.py
@@ -1,8 +1,18 @@
 palabras_feas = ["perro", "queso", "examen"]
 frase = input("Dime una frase:\n")
-
+"""
 for palabra in palabras_feas:
     frase = frase.replace(palabra, "*"*len(palabra))
 
 print(frase)
+"""
+
+# OPCION 2
+frase_separada = frase.split()
+nueva_frase = []
+for palabra in frase_separada:
+    if palabra in palabras_feas:
+        palabra = "*"*len(palabra)
+    nueva_frase.append(palabra)
+print(" ".join(nueva_frase))
 
