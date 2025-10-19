c0caf0e98111c05ca30b6a91cda1f16fb1223aa4
Cecilia <cecilia.delacamara@gmail.com>
2025-10-19 20:38:38 +0200

    ejercicio 4

---
+++ Clase16Octubre/Ejercicio4.py
@@ -0,0 +1,24 @@
+import os.path
+
+
+def copy_file(file1, file2):
+    if not os.path.exists(file1):
+        raise FileNotFoundError("El archivo a copiar no existe")
+
+    with open(file1, 'r') as file1:
+        lines_line1 = file1.readlines()
+        if len(lines_line1) == 0:
+            raise ValueError("El archivo a copiar está vacío")
+
+        if os.path.exists(file2):
+            answer = input(f"El archivo {file2} ya existe, ¿desea sobreescribir? Y/N\n")
+            if answer.upper() == "N":
+                return
+
+        with open(file2, 'w') as file2:
+            file2.writelines(lines_line1)
+
+file_to_copy = input("Introduce el nombre del archivo a copiar: \n")
+new_copied_file = input("Introduce el nombre del nuevo archivo: \n")
+
+copy_file(file_to_copy, new_copied_file)
