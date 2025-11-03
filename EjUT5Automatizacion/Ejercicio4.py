abf1a0660891dc3ce5bfe447e2ef5c7cc3de991f
Cecilia <cecilia.delacamara@gmail.com>
2025-11-03 20:09:54 +0100

    ejercicio 4

---
+++ EjUT5Automatizacion/Ejercicio4.py
@@ -0,0 +1,41 @@
+# Ejercicio 4 - Copiar el contenido de un archivo a otro¶
+# Escribe un programa que copie el contenido de un archivo de texto a otro archivo. El programa debe definir una función
+# que reciba los nombres de ambos archivos, lea el contenido del primer archivo y lo escriba en el segundo archivo.
+
+
+import os
+
+'''
+1- Comprobar que sea un archivo
+2- Comprobar que la nueva ruta es un directorio
+3- new_path = directorio/nombre_fichero
+5- Comprobar que path y new_path no son iguales
+6- Mover el archivo a new_path
+'''
+
+
+def move_file(path, new_path):
+    path_without_filename = new_path
+    new_filename = ""
+    if not os.path.isfile(path):
+        raise FileNotFoundError("No existe el archivo")
+    if not os.path.isdir(new_path):
+        path_without_filename = new_path.rsplit("/", maxsplit=1)[0]
+        new_filename = new_path.rsplit("/", maxsplit=1)[1]
+        if not os.path.isdir(path_without_filename):
+            raise FileNotFoundError("No existe el directorio de destino")
+
+    if len(new_filename) == 0:
+        # TODO
+        pass
+    else:
+        pass
+
+
+path = input("Introduce el archivo que quieres mover\n")
+new_path = input("Introduce la ruta donde quieres moverlo\n")
+
+try:
+    print(move_file(path, new_path))
+except Exception as e:
+    print(e)
